d05730c3062db437de7983bc558f5e47fc38a029
Ivan Leonidovich <main.line@live.ru>
2025-03-13 13:11:32 +0300

    Update data_models.py

--- models/data_models.py
+++ models/data_models.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
+from PyQt6.QtWidgets import QComboBox, QSpinBox
 
 class AlertLevel(IntEnum):
     NORMAL = 0
