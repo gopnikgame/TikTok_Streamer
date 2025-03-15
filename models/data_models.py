10a8f6f24843d4af951a91f2cdc3375224174dfa
Ivan Leonidovich <main.line@live.ru>
2025-03-15 16:13:37 +0300

    Update data_models.py

--- models/data_models.py
+++ models/data_models.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
 from PyQt6.QtWidgets import QComboBox, QSpinBox
+from utils.logger import Logger
 
 class AlertLevel(IntEnum):
     NORMAL = 0
@@ -13,9 +14,17 @@ class TableItemView:
     name: str
     event: str
     alert_level: AlertLevel = AlertLevel.NORMAL
+    
+    def __post_init__(self):
+        self.logger = Logger().get_logger('TableItemView')
+        self.logger.debug(f"Создан объект TableItemView: {self.timestamp} - {self.name} - {self.event} - {self.alert_level}")
 
 @dataclass
 class GiftData:
     id: int
     name: str
     image: str
+    
+    def __post_init__(self):
+        self.logger = Logger().get_logger('GiftData')
+        self.logger.debug(f"Создан объект GiftData: ID {self.id}, имя: {self.name}, изображение: {self.image[:20]}...")  # Обрезаем изображение для краткости лога
