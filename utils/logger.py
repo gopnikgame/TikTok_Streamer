d8adb107c322195a757b350967d6bda6d1e0b1d9
Ivan Yakunin <main.line@live.ru>
2025-03-15 20:56:47 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
+import locale
 
 class Logger:
     _instance = None
@@ -22,23 +23,45 @@ class Logger:
         log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         formatter = logging.Formatter(log_format)
         
-        # Настраиваем логирование в файл с ротацией
+        # Настраиваем логирование в файл с ротацией с явным указанием UTF-8
         file_handler = RotatingFileHandler(
             os.path.join(log_dir, "app.log"), 
             maxBytes=10*1024*1024,  # 10 МБ
-            backupCount=5
+            backupCount=5,
+            encoding='utf-8'
         )
         file_handler.setFormatter(formatter)
         file_handler.setLevel(logging.DEBUG)
         
-        # Настраиваем вывод в консоль
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setFormatter(formatter)
+        # Исправляем вывод в консоль для поддержки Unicode
+        # Проверяем текущую кодировку консоли
+        try:
+            # Для Windows установим кодировку консоли в UTF-8
+            if sys.platform == 'win32':
+                # Для Python 3.7+ можно использовать следующий способ:
+                sys.stdout.reconfigure(encoding='utf-8', errors='replace')
+                console_handler = logging.StreamHandler(sys.stdout)
+            else:
+                # Для других платформ просто используем стандартный обработчик
+                console_handler = logging.StreamHandler(sys.stdout)
+        except AttributeError:
+            # Для более старых версий Python, где метод reconfigure недоступен
+            console_handler = logging.StreamHandler(sys.stdout)
+            # Установим обработку ошибок кодирования на 'replace'
+            console_handler.setFormatter(logging.Formatter(log_format, None, 'replace'))
+        
         console_handler.setLevel(logging.INFO)
+        if not hasattr(console_handler, 'formatter'):
+            console_handler.setFormatter(formatter)
         
         # Создаем и настраиваем корневой логгер
         root_logger = logging.getLogger('TTStreamerPy')
         root_logger.setLevel(logging.DEBUG)
+        
+        # Удаляем все существующие обработчики перед добавлением новых
+        for handler in root_logger.handlers[:]:
+            root_logger.removeHandler(handler)
+            
         root_logger.addHandler(file_handler)
         root_logger.addHandler(console_handler)
         
