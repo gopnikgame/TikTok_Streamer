5ca6de2c341400bfbb145e59b6d5876595f2f203
Ivan Leonidovich <main.line@live.ru>
2025-03-17 10:27:40 +0300

    Update events_table_model.py

--- views/events_table_model.py
+++ views/events_table_model.py
@@ -1,4 +1,3 @@
-# views/events_table_model.py
 from PyQt6.QtCore import QAbstractTableModel, QModelIndex, Qt
 from PyQt6.QtGui import QStandardItemModel, QStandardItem, QPixmap, QImage
 from PyQt6.QtCore import QByteArray
@@ -10,38 +9,38 @@ class EventsTableModel(QAbstractTableModel):
     def __init__(self, viewmodel):
         super().__init__()
         self.viewmodel = viewmodel
-        self.headers = ["Время", "Пользователь", "Событие", "Уровень важности", "Подарок"]
+        self.headers = ["Р’СЂРµРјСЏ", "РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ", "РЎРѕР±С‹С‚РёРµ", "РЈСЂРѕРІРµРЅСЊ РІР°Р¶РЅРѕСЃС‚Рё", "РџРѕРґР°СЂРѕРє"]
         self.logger = Logger().get_logger('EventsTableModel')
-        self.logger.info("Инициализация модели таблицы событий")
+        self.logger.info("РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РјРѕРґРµР»Рё С‚Р°Р±Р»РёС†С‹ СЃРѕР±С‹С‚РёР№")
     
     def rowCount(self, parent=QModelIndex()):
-        self.logger.debug(f"Запрос количества строк: {len(self.viewmodel.item_list)}")
+        self.logger.debug(f"Р—Р°РїСЂРѕСЃ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚СЂРѕРє: {len(self.viewmodel.item_list)}")
         return len(self.viewmodel.item_list)
     
     def columnCount(self, parent=QModelIndex()):
-        self.logger.debug(f"Запрос количества столбцов: {len(self.headers)}")
+        self.logger.debug(f"Р—Р°РїСЂРѕСЃ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚РѕР»Р±С†РѕРІ: {len(self.headers)}")
         return len(self.headers)
     
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid() or not (0 <= index.row() < len(self.viewmodel.item_list)):
-            self.logger.debug(f"Неверный индекс: {index}")
+            self.logger.debug(f"РќРµРІРµСЂРЅС‹Р№ РёРЅРґРµРєСЃ: {index}")
             return None
         item = self.viewmodel.item_list[index.row()]
         if role == Qt.ItemDataRole.DisplayRole:
             if index.column() == 0:
-                self.logger.debug(f"Запрос данных для строки {index.row()}, столбца {index.column()}: {item.timestamp.strftime('%H:%M:%S')}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ РґР°РЅРЅС‹С… РґР»СЏ СЃС‚СЂРѕРєРё {index.row()}, СЃС‚РѕР»Р±С†Р° {index.column()}: {item.timestamp.strftime('%H:%M:%S')}")
                 return item.timestamp.strftime("%H:%M:%S")
             elif index.column() == 1:
-                self.logger.debug(f"Запрос данных для строки {index.row()}, столбца {index.column()}: {item.name}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ РґР°РЅРЅС‹С… РґР»СЏ СЃС‚СЂРѕРєРё {index.row()}, СЃС‚РѕР»Р±С†Р° {index.column()}: {item.name}")
                 return item.name
             elif index.column() == 2:
-                self.logger.debug(f"Запрос данных для строки {index.row()}, столбца {index.column()}: {item.event}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ РґР°РЅРЅС‹С… РґР»СЏ СЃС‚СЂРѕРєРё {index.row()}, СЃС‚РѕР»Р±С†Р° {index.column()}: {item.event}")
                 return item.event
             elif index.column() == 3:
-                self.logger.debug(f"Запрос данных для строки {index.row()}, столбца {index.column()}: {item.alert_level.name}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ РґР°РЅРЅС‹С… РґР»СЏ СЃС‚СЂРѕРєРё {index.row()}, СЃС‚РѕР»Р±С†Р° {index.column()}: {item.alert_level.name}")
                 return item.alert_level.name
             elif index.column() == 4:
-                self.logger.debug(f"Запрос данных для строки {index.row()}, столбца {index.column()}: {item.gift_name}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ РґР°РЅРЅС‹С… РґР»СЏ СЃС‚СЂРѕРєРё {index.row()}, СЃС‚РѕР»Р±С†Р° {index.column()}: {item.gift_name}")
                 return item.gift_name
         elif role == Qt.ItemDataRole.DecorationRole:
             if index.column() == 4:
@@ -50,13 +49,13 @@ class EventsTableModel(QAbstractTableModel):
                     return pixmap.scaled(50, 50, Qt.AspectRatioMode.KeepAspectRatio)
         elif role == Qt.ItemDataRole.BackgroundRole:
             if item.alert_level == AlertLevel.IMPORTANT:
-                self.logger.debug(f"Запрос фона для строки {index.row()} с уровнем важности: {item.alert_level}")
+                self.logger.debug(f"Р—Р°РїСЂРѕСЃ С„РѕРЅР° РґР»СЏ СЃС‚СЂРѕРєРё {index.row()} СЃ СѓСЂРѕРІРЅРµРј РІР°Р¶РЅРѕСЃС‚Рё: {item.alert_level}")
                 return Qt.GlobalColor.yellow
         return None
     
     def headerData(self, section, orientation, role):
         if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
-            self.logger.debug(f"Запрос заголовка для секции {section}: {self.headers[section]}")
+            self.logger.debug(f"Р—Р°РїСЂРѕСЃ Р·Р°РіРѕР»РѕРІРєР° РґР»СЏ СЃРµРєС†РёРё {section}: {self.headers[section]}")
             return self.headers[section]
         return None
     
